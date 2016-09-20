982aab59c6e2c1c4345a57dfe69a8993dcd4a6b5
suhe <hendarsyahss@gmail.com>
2016-09-20 07:49:56 -0700

    update module sales & customers

--- addons/partner_contact_kubik/models/res_partner.py
+++ addons/partner_contact_kubik/models/res_partner.py
@@ -3,7 +3,13 @@ from openerp import models,fields
 class ResPartner(models.Model) :
     _inherit = 'res.partner'
     pic = fields.Char()
+    pic_position = fields.Char(string="Position")
+    customer_from = fields.Selection([("Mandiri", "Mandiri"), ("Manajemen", "Manajemen")],
+                              string="From")
+    status = fields.Selection([("Prospek Client","Prospek Client"),("Existing Client","Existing Client")],string="Status")
     group_status = fields.Selection([('Cold', 'Cold'),
                                      ('Hot', 'Hot'),
                                      ('Not Qualifed', 'Not Qualifed'),
-                                     ('Warm','Warm')],string="Group Status")
+                                     ('Warm','Warm')],
+                                    string="Group Status")
+    phone_ext = fields.Char(string="Phone Extension")
